e365d140bc6b2e1d9d05a0a32eb71689f66d5b83
letrung0094 <letrung0094@hotmail.com>
2015-11-01 13:39:39 -0700

    Got application to only close when all instances have been closed

--- PS4/SpreadsheetGUI/Program.cs
+++ PS4/SpreadsheetGUI/Program.cs
@@ -16,7 +16,17 @@ namespace SpreadsheetGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var main = new Form1();
+            main.FormClosed += new FormClosedEventHandler(FormClosed);
+            main.Show();
+            Application.Run();
+        }
+
+        static void FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ((Form)sender).FormClosed -= FormClosed;
+            if (Application.OpenForms.Count == 0) Application.ExitThread();
+            else Application.OpenForms[0].FormClosed += FormClosed;
         }
     }
 }
