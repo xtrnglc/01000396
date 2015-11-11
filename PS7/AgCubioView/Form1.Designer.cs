cd0c9cfd1a3ac33c1f146ffbfe8b19d2991258e2
letrung0094 <letrung0094@hotmail.com>
2015-11-10 17:52:44 -0700

    commit

--- PS7/AgCubioView/Form1.Designer.cs
+++ PS7/AgCubioView/Form1.Designer.cs
@@ -28,21 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PlayerNameLabel = new System.Windows.Forms.Label();
+            this.PlayerNameTextBox = new System.Windows.Forms.TextBox();
+            this.ServerLabel = new System.Windows.Forms.Label();
+            this.ServerTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // PlayerNameLabel
+            // 
+            this.PlayerNameLabel.AutoSize = true;
+            this.PlayerNameLabel.Location = new System.Drawing.Point(152, 141);
+            this.PlayerNameLabel.Name = "PlayerNameLabel";
+            this.PlayerNameLabel.Size = new System.Drawing.Size(70, 13);
+            this.PlayerNameLabel.TabIndex = 0;
+            this.PlayerNameLabel.Text = "Player Name:";
+            // 
+            // PlayerNameTextBox
+            // 
+            this.PlayerNameTextBox.Location = new System.Drawing.Point(228, 138);
+            this.PlayerNameTextBox.Name = "PlayerNameTextBox";
+            this.PlayerNameTextBox.Size = new System.Drawing.Size(241, 20);
+            this.PlayerNameTextBox.TabIndex = 1;
+            // 
+            // ServerLabel
+            // 
+            this.ServerLabel.AutoSize = true;
+            this.ServerLabel.Location = new System.Drawing.Point(181, 184);
+            this.ServerLabel.Name = "ServerLabel";
+            this.ServerLabel.Size = new System.Drawing.Size(41, 13);
+            this.ServerLabel.TabIndex = 2;
+            this.ServerLabel.Text = "Server:";
+            this.ServerLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ServerTextBox
+            // 
+            this.ServerTextBox.Location = new System.Drawing.Point(228, 184);
+            this.ServerTextBox.Name = "ServerTextBox";
+            this.ServerTextBox.Size = new System.Drawing.Size(241, 20);
+            this.ServerTextBox.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 479);
+            this.Controls.Add(this.ServerTextBox);
+            this.Controls.Add(this.ServerLabel);
+            this.Controls.Add(this.PlayerNameTextBox);
+            this.Controls.Add(this.PlayerNameLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label PlayerNameLabel;
+        private System.Windows.Forms.TextBox PlayerNameTextBox;
+        private System.Windows.Forms.Label ServerLabel;
+        private System.Windows.Forms.TextBox ServerTextBox;
     }
 }
 
--- PS7/AgCubioView/Form1.cs
+++ PS7/AgCubioView/Form1.cs
@@ -21,5 +21,10 @@ namespace AgCubioView
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
